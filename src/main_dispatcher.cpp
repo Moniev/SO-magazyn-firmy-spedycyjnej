0fc97d20abcde000fd9a4b926c731def0bd31634
Robert Moń <m0ni3v@gmail.com>
2026-01-04 22:38:39 +0100

    feat: add dispatcher executable

--- src/main_dispatcher.cpp
+++ src/main_dispatcher.cpp
@@ -1,1 +1,19 @@
-int main() { return 0; }
+#include "../include/Manager.h"
+
+int main() {
+  Manager manager(false);
+
+  if (!manager.session_store->login("System-Dispatcher", UserRole::Operator, 0,
+                                    1)) {
+    return 1;
+  }
+
+  spdlog::info("[dispatcher] Ready to route packages.");
+
+  while (manager.getState()->running) {
+    manager.dispatcher->processNextPackage();
+  }
+
+  manager.session_store->logout();
+  return 0;
+}

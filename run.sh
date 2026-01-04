c684e6bfa6e670b937bbf260acefee39db121d66
Robert Moń <m0ni3v@gmail.com>
2026-01-04 22:56:50 +0100

    fix: solve most adequate cleanup after closing simulation

--- run.sh
+++ run.sh
@@ -4,6 +4,15 @@ CYAN='\033[36m'
 GREEN='\033[32m'
 RESET='\033[0m'
 
+remove_ipc() {
+  echo -e "${YELLOW}[sim] Removing IPC resources for user: $(whoami)...${RESET}"
+  ipcs -m | grep $(whoami) | awk '{print $2}' | xargs -r ipcrm -m 2>/dev/null
+  ipcs -s | grep $(whoami) | awk '{print $2}' | xargs -r ipcrm -s 2>/dev/null
+  ipcs -q | grep $(whoami) | awk '{print $2}' | xargs -r ipcrm -q 2>/dev/null
+}
+
+remove_ipc
+
 echo -e "${CYAN}[sim] Running binaries! ${RESET}"
 
 ./build/main &
@@ -28,9 +37,13 @@ echo -e "${GREEN}[sim] All of processes are currently running.${RESET}"
 echo "Press Ctrl+C, to stop whole simulation"
 
 cleanup() {
-  echo -e "\n${CYAN}[sim] Closing processes.${RESET}"
+  echo -e "\n${CYAN}[sim] Terminating processes...${RESET}"
   kill $BELT_PID $DISP_PID $TRUCK_PID $EXPR_PID $MASTER_PID 2>/dev/null
-  echo -e "${GREEN}[sim] Simulation closed.${RESET}"
+
+  sleep 0.5
+  remove_ipc
+
+  echo -e "${GREEN}[sim] Simulation closed and IPC resources freed.${RESET}"
   exit
 }
 

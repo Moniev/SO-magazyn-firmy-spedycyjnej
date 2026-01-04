8891b58f9c57815e6e50eb3b9b25e3363533a2d4
Robert Moń <m0ni3v@gmail.com>
2026-01-04 22:36:57 +0100

    feat: add master process executable

--- src/main.cpp
+++ src/main.cpp
@@ -1,1 +1,14 @@
-int main() { return 0; }
+#include "../include/Manager.h"
+
+int main() {
+  Manager manager(true);
+
+  spdlog::info("[ipc manager] Warehouse System Initialized.");
+
+  while (manager.getState()->running) {
+    std::this_thread::sleep_for(std::chrono::seconds(1));
+  }
+
+  spdlog::warn("[ipc manager] Shutdown signal received. Cleaning up.");
+  return 0;
+}

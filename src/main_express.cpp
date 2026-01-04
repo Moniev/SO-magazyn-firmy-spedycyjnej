77830b1ef265741396854e4dc6875f3c7bd6e327
Robert Moń <m0ni3v@gmail.com>
2026-01-04 22:39:12 +0100

    feat: add express executable

--- src/main_express.cpp
+++ src/main_express.cpp
@@ -1,1 +1,32 @@
-int main() { return 0; }
+#include "../include/Express.h"
+#include "../include/Manager.h"
+
+int main() {
+  Manager manager(false);
+
+  if (!manager.session_store->login("System-Express", UserRole::Operator, 0,
+                                    1)) {
+    return 1;
+  }
+
+  Express express(
+      manager.getState(), [&]() { manager.lockDock(); },
+      [&]() { manager.unlockDock(); }, [&]() { manager.lockBelt(); },
+      [&]() { manager.unlockBelt(); },
+      [&](SignalType s) { manager.sendSignal(s); });
+
+  spdlog::info("[express] VIP Handler ready.");
+
+  while (manager.getState()->running) {
+    SignalType sig = manager.receiveSignalBlocking();
+
+    if (sig == SIGNAL_EXPRESS_LOAD) {
+      express.deliverVipPackage();
+    } else if (sig == SIGNAL_END_WORK) {
+      break;
+    }
+  }
+
+  manager.session_store->logout();
+  return 0;
+}
